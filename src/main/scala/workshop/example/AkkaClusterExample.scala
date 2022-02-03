8d41072ab128acaac8279355c4b6e85059d34e56
Gopalakrishnan <gopalakrishnan.subramani@gmail.com>
2022-02-03 13:08:37 +0530

    membership

--- src/main/scala/workshop/example/AkkaClusterExample.scala
+++ src/main/scala/workshop/example/AkkaClusterExample.scala
@@ -2,6 +2,7 @@ package workshop.example
 
 
 import akka.actor.{Actor, ActorSystem, PoisonPill, Props}
+import akka.cluster.ClusterEvent.{InitialStateAsEvents, MemberDowned, MemberRemoved, MemberUp}
 import akka.pattern.ask
 import akka.util.Timeout
 
@@ -10,38 +11,61 @@ import scala.concurrent.duration._
 import scala.language.postfixOps
 import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
-import akka.cluster.{Cluster, Member, MemberStatus, ClusterEvent}
+import akka.cluster.{Cluster, ClusterEvent, Member, MemberStatus}
 
 
 object AkkaClusterExample extends  App {
-  println("hello actor created")
+
 
   class HelloActor extends Actor {
-  // actor path, tree hierarchy , print the path
-  // actor name is used at end path
+    println("hello actor created")
   println("Actor path ", akka.serialization.Serialization.serializedActorPath(self))
-  // every actor should have receive method
-  // this function is called on every message
-  // who will call this method, called by dispatcher
-  // dispatcher will fetch message from mailbox, get a  thread from pool, call receive on the thread
-  def receive = {
-    // handle ask
-    // sender represent who ask/send message
-    // self is hello actor reference
+
+    // if the actors wants to receive notificaiton on member up, down, removed, events
+    // we need to susbcribe from cluser
+    // get reference to cluster
+    val cluster = Cluster(context.system)
+
+    def receive = {
+
+    case MemberUp(m) => {
+      println("New Member up ", m)
+    }
+    case MemberDowned(m) => {
+      println("Member Down ", m)
+    }
     case "what is time now?" => sender.tell(System.nanoTime(), self)
     //handle tell
     case msg: String => println("message is", msg)
     case _ => println("there is a msg")
   }
 
+    override def preStart(): Unit = {
+      super.preStart()
+      println("HelloActor PresStart")
+
+      // self mean, this /current actor/hello actor
+      cluster.subscribe(self, initialStateMode = InitialStateAsEvents,
+        classOf[MemberUp],
+        classOf[MemberDowned],
+
+        classOf[MemberRemoved],
+
+      )
+    }
 
 } // HelloActor
 
-// create actor system
-// every actor system will have name
 val system = ActorSystem("training")
 
   // create a cluster
 
 val cluster = Cluster(system)
+
+  // we need to register the actors after cluster Member up
+  cluster registerOnMemberUp {
+     println("on register phase")
+    // akka://training@127.0.0.1:2551/user/helloworld#-2137257253
+     system.actorOf(Props[HelloActor], name="helloworld")
+  }
 }
